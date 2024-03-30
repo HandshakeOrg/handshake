faec57d16978b0ce1c09a8422ff8762a53a165af
Dominic <chinonsodomnic@gmail.com>
2024-03-31 00:14:38 +0100

    Update app.py

--- server/app.py
+++ server/app.py
@@ -2,19 +2,10 @@
 """
 Start Handshake web application
 """
-from server.models import User
-from server import app, db, login_manager
+from server import app, db
 from flask_cors import CORS
 from server.views import app_views
 from flask import jsonify
-
-@login_manager.user_loader
-def load_user(user_id):
-    """
-    wrapper function
-    loads a user from the database by the user_id
-    """
-    return User.query.get(int(user_id))
 
 # Creates all tables if it does not exist
 with app.app_context():
