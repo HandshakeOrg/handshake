02d1593eb31c5fa19b4de51a57e418f0d1003eea
Faruk <adekoladamilola4@gmail.com>
2024-04-03 23:44:21 +0100

    solve the reloading issue

--- client/app/src/contexts/AuthContext.jsx
+++ client/app/src/contexts/AuthContext.jsx
@@ -1,12 +1,18 @@
-import { createContext, useContext, useReducer, useState } from "react";
-import { toast } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
-import PropTypes from "prop-types";
+import {
+  createContext,
+  useContext,
+  useReducer,
+  useState,
+  useEffect,
+} from 'react';
+import { toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import PropTypes from 'prop-types';
 
 const AuthContext = createContext();
 
 // const BASE_URL = 'https://handshake-edac.onrender.com/api';
-const BASE_URL = "http://localhost:5000/api";
+const BASE_URL = 'http://localhost:5000/api';
 
 let initialState = {
   user: null,
@@ -15,49 +21,62 @@ let initialState = {
 
 function reducer(state, action) {
   switch (action.type) {
-    case "createAccount":
+    case 'createAccount':
       return { ...state, user: action.payload };
-    case "login":
+    case 'login':
       return { ...state, user: action.payload, isAuthenticated: true };
-    case "logout":
+    case 'logout':
       return { ...state, user: action.payload, isAuthenticated: false };
-    case "deleteAccount":
+    case 'deleteAccount':
       return { ...state, user: action.payload, isAuthenticated: false };
     default:
-      throw new Error("Unknown action");
+      throw new Error('Unknown action');
   }
 }
 
 function AuthProvider({ children }) {
+  const [loading, setLoading] = useState(false);
   const [{ user, isAuthenticated }, dispatch] = useReducer(
     reducer,
     initialState,
+    () => {
+      // Initialize state from localStorage
+      const storedState = localStorage.getItem('authState');
+      return storedState ? JSON.parse(storedState) : initialState;
+    }
   );
-  const [loading, setLoading] = useState(false);
+
+  // Save state to localStorage whenever it changes
+  useEffect(() => {
+    localStorage.setItem(
+      'authState',
+      JSON.stringify({ user, isAuthenticated })
+    );
+  }, [user, isAuthenticated]);
 
   async function createAccount(data) {
     try {
       setLoading(true);
       const response = await fetch(`${BASE_URL}/signup`, {
-        method: "POST",
-        credentials: "include",
+        method: 'POST',
+        credentials: 'include',
         headers: {
-          "Content-Type": "application/json",
+          'Content-Type': 'application/json',
         },
         body: JSON.stringify(data),
       });
       console.log(response);
       if (!response.ok) {
         const errorData = await response.json();
-        console.log("Server error:", errorData);
+        console.log('Server error:', errorData);
         toast.error(errorData.error);
         throw new Error(errorData.error.message);
       }
 
       const ResponseData = await response.json();
       console.log(ResponseData);
-      dispatch({ type: "createAccount", payload: ResponseData });
-      toast.success("Account created successfully");
+      dispatch({ type: 'createAccount', payload: ResponseData });
+      toast.success('Account created successfully');
       setLoading(false);
     } catch (error) {
       console.error(error);
@@ -70,18 +89,18 @@ function AuthProvider({ children }) {
     try {
       setLoading(true);
       const formData = new FormData();
-      formData.append("email", email);
-      formData.append("password", password);
+      formData.append('email', email);
+      formData.append('password', password);
 
       const response = await fetch(`${BASE_URL}/login`, {
-        method: "POST",
-        credentials: "include",
+        method: 'POST',
+        credentials: 'include',
         body: formData,
       });
 
       if (response.ok) {
         const data = await response.json();
-        dispatch({ type: "login", payload: data.current_user });
+        dispatch({ type: 'login', payload: data.current_user });
         setLoading(false);
       } else {
         const errorData = await response.json();
@@ -91,29 +110,29 @@ function AuthProvider({ children }) {
       }
     } catch (error) {
       setLoading(false);
-      toast.error("Something went wrong. Please try again.");
+      toast.error('Something went wrong. Please try again.');
     }
   }
   async function deleteAccount(user) {
     try {
       setLoading(true);
       const formData = new FormData();
-      formData.append("user", user.id);
+      formData.append('user', user.id);
 
       fetch(`${BASE_URL}/settings/delete`, {
-        credentials: "include",
-        method: "DELETE",
+        credentials: 'include',
+        method: 'DELETE',
         body: formData,
       })
         .then((response) => response.json())
         .then((data) => {
-          dispatch({ type: "deleteAccount", payload: data });
-          toast.success("Account deleted successfully");
+          dispatch({ type: 'deleteAccount', payload: data });
+          toast.success('Account deleted successfully');
         })
         .catch((error) => console.log(error)); // This is a general catch block for any errors that might occur during the fetch request.
     } catch (error) {
       console.error(error); // This line logs the error to the console.
-      toast.error("Something went wrong. Please try again."); // This line shows a toast error message.
+      toast.error('Something went wrong. Please try again.'); // This line shows a toast error message.
     } finally {
       setLoading(false); // This line sets the loading state to false after the API call, regardless of whether it was successful or not.
     }
@@ -122,23 +141,23 @@ function AuthProvider({ children }) {
   async function logout() {
     try {
       setLoading(true);
-      console.log("Attempting to log out...");
+      console.log('Attempting to log out...');
 
       const response = await fetch(`${BASE_URL}/logout`, {
-        credentials: "include",
-        method: "GET",
+        credentials: 'include',
+        method: 'GET',
       });
 
       if (!response.ok) {
-        const errorMessage = response.statusText || "An error occurred";
+        const errorMessage = response.statusText || 'An error occurred';
         throw new Error(errorMessage);
       }
 
-      dispatch({ type: "logout", payload: {} }); // Empty payload
-      console.log("Logout successful");
+      dispatch({ type: 'logout', payload: {} }); // Empty payload
+      console.log('Logout successful');
     } catch (error) {
-      console.error("Logout failed", error);
-      toast.error("Something went wrong. Please try again.");
+      console.error('Logout failed', error);
+      toast.error('Something went wrong. Please try again.');
     } finally {
       setLoading(false);
     }
@@ -163,7 +182,7 @@ function AuthProvider({ children }) {
 function useAuth() {
   const context = useContext(AuthContext);
   if (context === undefined)
-    throw new Error("AuthContext was use outside the auth provider");
+    throw new Error('AuthContext was use outside the auth provider');
   return context;
 }
 
